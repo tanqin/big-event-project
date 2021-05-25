f247ae1a7e0bb3911edc15392f96964dcf898e53
tanqin <tagtan@qq.com>
2021-05-25 18:12:09 +0800

    添加文章类别管理部分内容，jqconfig.js修改url默认格式

--- admin/js/jqconfig.js
+++ admin/js/jqconfig.js
@@ -2,5 +2,6 @@ $.ajaxPrefilter(function (option, originalOptions, jqXHR) {
   option.headers = {
     Authorization: localStorage.getItem("usertoken"), //从本地存储中获取到用户登录后的token携带给服务器
   };
-  console.log(option);
+  option.url = option.url.indexOf("http") !== -1 ? option.url : "http://localhost:8080/api/v1" + option.url;
+  //   console.log(option);
 });
